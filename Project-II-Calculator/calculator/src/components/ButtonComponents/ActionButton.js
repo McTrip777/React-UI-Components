df3d94fafe2411c36db40cfbdad9ffb6fcb17e05
McTrip777 <mcjakedude@gmail.com>
2019-02-12 12:06:25 -0800

    first calc commit

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,30 @@
 import React from 'react';
 import './Button.css';
+
+
+const ActionButton = () => {
+    return (
+      <div className='action'>
+        <div className='actionBox actionbox1'>
+        <p>clear</p>
+      </div>
+      <div className='actionBox actionbox2'>
+        <p>/</p>
+      </div>
+      <div className='actionBox actionbox3'>
+        <p>x</p>
+      </div>
+      <div className='actionBox actionbox4'>
+        <p>-</p>
+      </div>
+      <div className='actionBox actionbox5'>
+        <p>+</p>
+      </div>
+      <div className='actionBox actionbox6'>
+        <p>=</p>
+      </div>
+      </div>
+    );
+};
+  
+  export default ActionButton;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,42 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = () => {
+    return (
+        <div className='buttonBoxes'>
+        <div className='box box7'>
+          <p>7</p>
+        </div>
+        <div className='box box8'>
+          <p>8</p>
+        </div>
+        <div className='box box9'>
+          <p>9</p>
+        </div>
+        <div className='box box4'>
+          <p>4</p>
+        </div>
+        <div className='box box5'>
+          <p>5</p>
+        </div>
+        <div className='box box6'>
+          <p>6</p>
+        </div>
+        <div className='box box1'>
+          <p>1</p>
+        </div>
+        <div className='box box2'>
+          <p>2</p>
+        </div>
+        <div className='box box3'>
+          <p>3</p>
+        </div>
+        <div className='box box0'>
+          <p>0</p>
+        </div>
+       </div>
+    );
+  };
+  
+  export default NumberButton;
+  
