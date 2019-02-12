a4cb85a5c22d35f6422472fba56ce957dd23d336
McTrip777 <mcjakedude@gmail.com>
2019-02-12 12:49:49 -0800

    alignment

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -5,24 +5,26 @@ import './Button.css';
 const ActionButton = () => {
     return (
       <div className='action'>
-        <div className='actionBox actionbox1'>
-        <p>clear</p>
-      </div>
-      <div className='actionBox actionbox2'>
-        <p>/</p>
-      </div>
-      <div className='actionBox actionbox3'>
-        <p>x</p>
-      </div>
-      <div className='actionBox actionbox4'>
-        <p>-</p>
-      </div>
-      <div className='actionBox actionbox5'>
-        <p>+</p>
-      </div>
-      <div className='actionBox actionbox6'>
-        <p>=</p>
-      </div>
+        <div className='clear'>
+            <p>clear</p>
+        </div>
+        <div>
+            <div className='actionBox actionbox1'>
+                <p>/</p>
+            </div>
+            <div className='actionBox actionbox2'>
+                <p>x</p>
+            </div>
+            <div className='actionBox actionbox3'>
+                <p>-</p>
+            </div>
+            <div className='actionBox actionbox4'>
+                <p>+</p>
+            </div>
+            <div className='actionBox actionbox5'>
+                <p>=</p>
+            </div>
+        </div>
       </div>
     );
 };
